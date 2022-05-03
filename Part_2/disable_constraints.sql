020e182460affec227c24b0e6d5d8fe952bbaa63
TemryL <tommery01@gmail.com>
2022-05-04 00:15:10 +0200

    enable/disable constraints

---
+++ Part_2/disable_constraints.sql
@@ -0,0 +1,13 @@
+BEGIN
+    FOR c IN
+    (SELECT c.owner, c.table_name, c.constraint_name
+        FROM user_constraints c, user_tables t
+        WHERE c.table_name = t.table_name
+        AND c.status = 'ENABLED'
+        AND NOT (t.iot_type IS NOT NULL AND c.constraint_type = 'P')
+        ORDER BY c.constraint_type DESC)
+    LOOP
+        dbms_utility.exec_ddl_statement('alter table "' || c.owner || '"."' || c.table_name || '"
+        disable constraint ' || c.constraint_name);
+    END LOOP;
+END; 
